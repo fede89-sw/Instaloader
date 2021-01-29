f32ab88032873cb00d64eff57e4baf02d79cd29b
Federico Milani <76585274+fede89-sw@users.noreply.github.com>
2021-01-29 23:52:01 +0100

    Add files via upload

--- insta_loader.py
+++ insta_loader.py
@@ -1,23 +1,21 @@
 from instaloader.instaloader import Instaloader
 from instaloader.structures import Profile
 
-from itertools import islice
-from math import ceil
 
-L = Instaloader()
+L = Instaloader(download_video_thumbnails=False, download_geotags=False, download_comments=False, save_metadata=False, compress_json=False)
 
 
 class Instagram:
 
     def __init__(self):
-        self. target = ""
-        self.user = ""
+        self.target = ""
+        self.user = "89fede_"
+        self.profilo = ""
 
 
     def get_posts(self):
         """ ottengo tutti i post di un profilo, dato username - LOGIN REQUIRED """
-        profile = Profile.from_username(L.context, self.target)
-        posts = profile.get_posts()
+        posts = self.profilo.get_posts()
         L.interactive_login(self.user)
         for post in posts:  
             try:            
@@ -25,80 +23,91 @@ class Instagram:
             except KeyboardInterrupt:
                 print("\nexiting...")
                 break
+            except:
+                print("Errore nel Download dei Posts!")
 
 
-    def get_user_id(self):
-        """ ottengo ID di in profilo, dato username """
-        return Profile.from_username(L.context, self.target)
+    @staticmethod
+    def get_user_profile(target):
+        """ ottengo Profilo, dato username """
+        return Profile.from_username(L.context, target)
 
 
     def download_stories(self):
         """ DOWNLOAD STORIES -> LOGIN REQUIRED """
-        user_id = self.get_user_id()
-        if user_id.has_public_story:
+        if self.profilo.has_public_story:
             try:
                 L.interactive_login(self.user)
-                L.download_stories(userids=[user_id], filename_target=f"{self.target}") 
+                L.download_stories(userids=[self.profilo], filename_target=f"{self.target}") 
             except KeyboardInterrupt:
                 print("\nexiting...")
         else:
-            print(f"{self.target} non ha storie pubbliche al momento!")
+            print(f"{self.profilo} non ha storie pubbliche al momento!")
 
 
     def download_igtv(self):
         """ Scarico IGTV """
-        profile = Profile.from_username(L.context, self.target)
-        try:
-            print("Sto scaricando...")
-            L.download_igtv(profile=profile)
-        except KeyboardInterrupt:
-            print("\nexiting...")       
+        if self.profilo.igtvcount != 0:
+            try:
+                print("Sto scaricando...")
+                L.download_igtv(profile=self.profilo)
+            except KeyboardInterrupt:
+                print("\nexiting...")
+        else:
+            print("Non ci sono IGTV video da scaricare...")    
 
 
     def get_profile_picture(self):
         """ Scarico Foto Profilo """
-        profile = Profile.from_username(L.context, self.target)
-        L.download_profilepic(profile=profile)
+        L.download_profilepic(profile=self.profilo)
+
+
+    def get_profile_info(self):
+        try:
+            print(f"""
+    {self.profilo.username}:
+    
+Nome: {self.profilo.full_name}
+ID: {self.profilo.userid}
+Followers: {self.profilo.followers}
+Seguiti: {self.profilo.followees}
+Bio: {self.profilo.biography}
+Verificato: {self.profilo.is_verified}
+Storie Pubbliche: {self.profilo.has_public_story}
+Privato: {self.profilo.is_private}
+            """)            
+        except:
+            print("Errore...")
 
 
     def start(self):
-        self.user = input("Inserisci il tuo Username: ")
-        self.target = input("inserisci l'Username Target: ")
-        scelta = input("""\n
-            SELEZIONA AZIONE DA ESEGUIRE:
-            1- SCARICA TUTTI I POST DI UN 'TARGET' (COMPRESI IGTV E VIDEO)
-            2- SCARICA LE STORIE ATTUALI DI UN 'TARGET'
-            3- SCARICA IGTV DI 'TARGET'
-            4- SCARICA FOTO PROFILO DI 'TARGET'
-            """)
-        if scelta == "1":
-            self.get_posts()
-        elif scelta == "2":
-            self.download_stories()
-        elif scelta == "3":
-            self.download_igtv()
-        elif scelta == "4":
-            self.get_profile_picture()
+        try:
+            # self.user = input("Inserisci il tuo Username: ")
+            self.target = input("inserisci l'Username Target: ")
+            self.profilo = self.get_user_profile(self.target)
+            scelta = input("""\n
+SELEZIONA AZIONE DA ESEGUIRE:
+1- SCARICA TUTTI I POST DI UN 'TARGET' (COMPRESI IGTV E VIDEO)
+2- SCARICA LE STORIE ATTUALI DI UN 'TARGET'
+3- SCARICA IGTV DI 'TARGET'
+4- SCARICA FOTO PROFILO DI 'TARGET'
+5- INFORMAZIONI SUL PROFILO
+    """)
+        except:
+            print("Errore nell'inserimento dati!")
         else:
-            print("Scelta non valida!")
-
-
-
-""" Scopro se il Profilo è privato """
-# profile = Profile.from_username(L.context, '89fede_')
-# print(profile.is_private)
-
-
-""" URL foto profilo """
-# profile = Profile.from_username(L.context, 'sara_mory')
-# print(profile.profile_pic_url)
-
-
-""" get_posts()/get_saved_posts()/get_igtv_posts()/get_followees() """
-# user = '89fede_'
-# L.interactive_login(user)
-# profile = Profile.from_username(L.context, 'sara_mory')
-# print(profile.get_followees())
+            if scelta == "1":
+                self.get_posts()
+            elif scelta == "2":
+                self.download_stories()
+            elif scelta == "3":
+                self.download_igtv()
+            elif scelta == "4":
+                self.get_profile_picture()
+            elif scelta == "5":
+                self.get_profile_info()
+            else:
+                print("Scelta non valida!")
 
 
 if __name__ == "__main__":
